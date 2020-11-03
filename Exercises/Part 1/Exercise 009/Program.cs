19c7c56726cf0921a7465364930b087e8e79b6cb
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-03 23:33:17 +0200

    03/11/2020

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -6,8 +6,11 @@ namespace exercise_09
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
-
+            Console.Write("Hello, how are you?");
+            string message = Console.ReadLine();
+            Console.Write("That's interesting, tell me more");
+            string message = Console.ReadLine();
+            Console.Write("Thank you for sharing!");
         }
     }
 }
