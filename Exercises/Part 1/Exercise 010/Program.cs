d09cc3513fac1081cbabbb0e52fcdaed97d3ae9f
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-03 23:48:57 +0200

    03/11/2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -6,12 +6,16 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-          
-
-           Console.Write(kdfkfj);
-
-
-
+            Console.WriteLine("I will tell a story, but I need some information.\n"
+            "Give a name for main character: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Give the character a profession: ");
+            string prof = Console.ReadLine();
+            Console.WriteLine("Here is the story:\n"
+            "Once upon a time there was a " + prof + " called " + name +"\n"
+            "On her way to work, " + name + "often pondered what being " + prof + "meant to them.\n"
+            "When you work as a" + prof + "you meet interesting people.\n"
+            name + " enjoys their work as " + prof + ", The end.");
         }
     }
 }
