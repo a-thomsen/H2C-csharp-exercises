4f4decff42a53757eeeea59c2d16510a2b4181c2
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-05 00:02:24 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,41 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.Write("Fill in your grading percentage");
+      string input = Console.ReadLine();
+      int grade = Convert.ToInt32(input);
+      if (grade < 0) 
+      {
+        Console.WriteLine("Impossible!");
+      }
+      else if (grade <= 49) 
+      {
+        Console.WriteLine("Fail");
+      }   
+      else if (grade <= 59) 
+      {
+        Console.WriteLine("Grade is: 1");
+      } 
+      else if (grade <= 69) 
+      {
+        Console.WriteLine("Grade is: 2");
+      } 
+      else if (grade <= 79) 
+      {
+        Console.WriteLine("Grade is: 3");
+      } 
+      else if (grade <= 89) 
+      {
+        Console.WriteLine("Grade is: 4");
+      } 
+      else if (grade <= 100) 
+      {
+        Console.WriteLine("Grade is: 5");
+      }
+      else
+      {
+        Console.WriteLine("Outstanding!");
+      }
     }
   }
 }
