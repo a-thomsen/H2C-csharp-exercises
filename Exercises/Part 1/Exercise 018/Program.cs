23e32a923d69a53d22144565a18c4820301b6534
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 20:41:47 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give me a number!");
+      string firstStr = Console.ReadLine();
+      int first = Convert.ToInt32(firstStr);
+      Console.WriteLine("Give me another number!");
+      string secondStr = Console.ReadLine();
+      int second = Convert.ToInt32(secondStr);
+      Console.WriteLine("Now give me one more number!");
+      string thirdStr = Console.ReadLine();
+      int third = Convert.ToInt32(thirdStr);
+      Console.WriteLine("The sum of these numbers is " + (first + second + third));
 
     }
   }
