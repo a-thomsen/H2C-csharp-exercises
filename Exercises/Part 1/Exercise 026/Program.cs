d7e8e833615f7b151cb817e67df473f5a6c590af
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 23:38:22 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 026/Program.cs
+++ Exercises/Part 1/Exercise 026/Program.cs
@@ -6,8 +6,13 @@ namespace exercise_26
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.Write("Give your birth year");
+      string input = Console.ReadLine();
+      int year = Convert.ToInt32(input);
+      if (year < 1900) 
+      {
+        Console.WriteLine("You're old");
+      }
     }
   }
 }
