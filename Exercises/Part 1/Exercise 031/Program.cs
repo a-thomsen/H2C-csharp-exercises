da8dea5e20fb9bbcfb2a31b8b6938374ee7a43b5
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-05 00:08:31 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,8 +6,17 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.Write("Give a number");
+      string input = Console.ReadLine();
+      int number = Convert.ToInt32(input);
+      if ((number % 2) == 0) 
+      {
+        System.out.println("The number is even.");
+      } 
+      else 
+      {
+        System.out.println("The number is odd.");
+      }
     }
   }
 }
