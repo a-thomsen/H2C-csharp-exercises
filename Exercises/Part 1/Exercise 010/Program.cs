7bb688793942446214f46dda995ee184f9d0819f
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 11:09:40 +0200

    03/11/2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -11,10 +11,10 @@ namespace exercise_10
             string name = Console.ReadLine();
             Console.WriteLine("Give the character a profession: ");
             string prof = Console.ReadLine();
-            Console.WriteLine("Here is the story:\n"
-            "Once upon a time there was a " + prof + " called " + name +"\n"
-            "On her way to work, " + name + "often pondered what being " + prof + "meant to them.\n"
-            "When you work as a" + prof + "you meet interesting people.\n"
+            Console.WriteLine("Here is the story:\n" +
+            "Once upon a time there was a " + prof + " called " + name +"\n" +
+            "On her way to work, " + name + "often pondered what being " + prof + "meant to them.\n" +
+            "When you work as a" + prof + "you meet interesting people.\n" +
             name + " enjoys their work as " + prof + ", The end.");
         }
     }
--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,8 +7,21 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine("Give a string: ");
+        string str = Console.ReadLine();
+      Console.WriteLine("Give an integer: ");
+        string userInt = Console.ReadLine();
+        int intValue = Convert.ToInt32(userInt);
+      Console.WriteLine("Give a double: ");
+        string userDoub = Console.ReadLine();
+        double doubleValue = Convert.ToDouble(userDoub);
+      Console.WriteLine("Give me the truth!");
+        string userBool = Console.ReadLine();
+        bool booleanValue = System.Convert.ToBoolean(userBool);
+      Console.WriteLine("Your string: " + str + "\n" +
+      "Your integer: " + intValue + "\n" +
+      "Your double: " + doubleValue + "\n" +
+      "Your boolean: " + booleanValue);
     }
   }
 }
