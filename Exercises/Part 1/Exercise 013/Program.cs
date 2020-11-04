ce7721401f42fdbf26af36a4f43de04db6c1837f
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 10:33:30 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 013/Program.cs
+++ Exercises/Part 1/Exercise 013/Program.cs
@@ -6,7 +6,9 @@ namespace exercise_13
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("Give a double!");
+      double value = Console.ReadLine();
+      Console.Write("You gave " + value);
 
     }
   }
