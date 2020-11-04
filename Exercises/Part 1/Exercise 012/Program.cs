b6573843975a6c1638b7cf3848a18933d579715e
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 10:39:40 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 012/Program.cs
+++ Exercises/Part 1/Exercise 012/Program.cs
@@ -7,8 +7,8 @@ namespace exercise_12
     public static void Main(string[] args)
     {
       Console.Write("Give an integer!");
-      int value = Console.ReadLine();
-      Console.Write("You gave " + value);
+      int intValue = Convert.ToInt32(userInput);
+      Console.Write("You gave " + intValue);
     }
   }
 }
