5fae5b11dc1bd8c9facba6a2bae8d19fbd0cdb47
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 23:45:23 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 028/Program.cs
+++ Exercises/Part 1/Exercise 028/Program.cs
@@ -6,8 +6,17 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.Write("How old are you?");
+      string input = Console.ReadLine();
+      int age = Convert.ToInt32(input);
+      if (age >= 18) 
+      {
+        Console.WriteLine("You're an adult!");
+      }
+      else 
+      {
+        Console.WriteLine("You're underaged!");
+      }
     }
   }
 }
