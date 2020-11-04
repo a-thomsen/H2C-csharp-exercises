94da7fc5d1e80c5b7984c8a2efe485ef83704883
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-04 20:51:40 +0200

    04/11/2020

--- Exercises/Part 1/Exercise 020/Program.cs
+++ Exercises/Part 1/Exercise 020/Program.cs
@@ -6,9 +6,13 @@ namespace exercise_20
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
-
+      Console.WriteLine("Give me a number!");
+      string firstStr = Console.ReadLine();
+      int first = Convert.ToInt32(firstStr);
+      Console.WriteLine("Give me another number!");
+      string secondStr = Console.ReadLine();
+      int second = Convert.ToInt32(secondStr);
+      Console.WriteLine(first " * " second " = " (first * second); 
     }
   }
 }
