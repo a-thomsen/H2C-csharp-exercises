589719dd985649c87dba2c32b8b390fb7f114f67
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-06 22:49:47 +0200

    06/11/2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,21 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      while (true) 
+      {
+        Console.WriteLine("Type in a number, 0 quits");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number == 0) 
+        {
+          break;
+        }
+        if  (number < 0)
+        {
+          Console.WriteLine("That is a negative");
+          continue;
+        }
+      Console.WriteLine("Your number raised in its second power is " + (number * number));
+      }
     }
   }
 }
