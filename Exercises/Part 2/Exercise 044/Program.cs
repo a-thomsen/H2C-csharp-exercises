16e3a7ab4d11bee3d2bc8a630c046fa094e043f5
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-07 13:39:57 +0200

    07/11/2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,8 +7,23 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Hit me with a number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Hit me with another number!");
+      int second = Convert.ToInt32(Console.ReadLine());
 
+      if (first > second)
+      {
+        Console.WriteLine(first + " is greater than " + second);
+      }
+      else if (first < second)
+      {
+        Console.WriteLine(first + " is less than " + second);
+      }
+      else
+      {
+        Console.WriteLine(first + " is equal to " + second);
+      }
     }
   }
 }
