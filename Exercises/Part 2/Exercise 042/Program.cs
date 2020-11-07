df096608646f0d764fd51ebf7766998ea022d893
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-07 13:28:33 +0200

    07/11/2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -7,8 +7,16 @@ namespace exercise_42
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-    
+      Console.WriteLine("Hit me with a number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Hit me with another number!");
+      int second = Convert.ToInt32(Console.ReadLine());
+
+      int sum = first + second;
+
+      double sqRoot = Math.Sqrt(sum);
+
+      Console.WriteLine("The square root of the sum of your inputs is " + sqRoot);
     }
   }
 }
