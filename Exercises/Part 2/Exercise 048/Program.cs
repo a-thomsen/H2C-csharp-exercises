7a6f75cb8c8a5a8e9fbf1da4432af06b50651e71
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-09 01:58:35 +0200

    08/11/2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -7,8 +7,40 @@ namespace exercise_48
     public static void Main(string[] args)
     {
 
-      // Write your code here:
- 
+      Console.WriteLine("Write numbers: ");
+      int sum = 0;
+      int amount = 0;
+      int even = 0;
+      int odd = 0; 
+      double average = Convert.ToDouble(sum / amount);
+
+      while(true)
+      {
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number == -1)
+        {
+          break;
+          Console.WriteLine("Thx! Bye!"); 
+        }
+        if ((number % 2) == 0)
+        {
+          even++;
+          continue;
+        }
+        else
+        {
+          odd++;
+          continue;
+        }
+
+
+      sum += number;   
+      amount++;
+      }
+     Console.WriteLine("The sum of the numbers is " + sum +"\n"
+     "The total of numbers typed was " + amount + " and the average of them " + average +"\n"
+     "Out of the numbers " + even + " were even, and " + odd + " were odd.");
     }
   }
 }
