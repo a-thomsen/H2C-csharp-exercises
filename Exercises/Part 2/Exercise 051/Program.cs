9a7e5cfc11f5bbd62598b9970e63bb07db270f9a
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-09 23:56:27 +0200

    09/11/2020

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -6,12 +6,21 @@ namespace exercise_51
   {
     public static void Main(String[] args)
     {
+      Console.WriteLine("Give me a number and I'll do the counting!");
+      int max = Convert.ToInt32(Console.ReadLine());
       // Call your method here:
-
-
+      PrintUntilNumber(max);
     }
 
     // Write your method here:
-
+    public static void PrintUntilNumber (int max)
+    {
+      int min = 1;
+      while (min < max) 
+      {
+          Console.WriteLine(min);
+          min++;
+      }
+    }  
   }
 }
