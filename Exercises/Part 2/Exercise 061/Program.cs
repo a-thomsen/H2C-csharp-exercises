d3e7fa24cbc80afb4860fcb0d40f6b1ec80dccb2
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-10 21:55:00 +0200

    10/11/2020

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,49 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
+      int number = Convert.ToInt32(Console.ReadLine());
 
     }
 
     public static void PrintStars(int number)
     {
-
+      for(int i = 0; i <= number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
     }
 
     public static void PrintSpaces(int number)
     {
-
+      for (int i = 0; i <= number; i++)
+      {
+        Console.Write(" ");
+      }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+      for (int i = 1; i <= size; i++)
+      {
+        for (int x = 1; x <= i; x++)
+        {
+          Console.Write(" ");
+        }
+        for (int y = 1; y <= i; y++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");
+      }
     }
 
     public static void ChristmasTree(int height)
     {
-
+      for (int i = 0; i <= height; i++)
+      {
+        // Continue from here
+      }
     }
   }
 }
