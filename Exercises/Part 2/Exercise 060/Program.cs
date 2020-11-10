f6525d1dc7e3cadb0af90944e09ea1812a004b77
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-10 21:32:08 +0200

    10/11/2020

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -6,32 +6,63 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
+        int number = Convert.ToInt32(Console.ReadLine());
+        PrintStars(number);
 
+        int size = Convert.ToInt32(Console.ReadLine());
+        PrintSquare(size);
+
+        int width = Convert.ToInt32(Console.ReadLine());
+        int height = Convert.ToInt32(Console.ReadLine());
+        PrintRectangle(width, height);
+
+        PrintTriangle(size);
     }
 
 
     public static void PrintStars(int number)
     {
-      // you can print one star with the command
-      // Console.Write("*");
-      // call the print command n times
-      // in the end print a line break with the comand
-      // Console.WriteLine("");
+      for(int i = 0; i <= number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
     }
 
     public static void PrintSquare(int size)
     {
-
+      for (int i = 0; i <= size; i++)
+      {
+        for (int x = 0; x <= size; x++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");
+      }
     }
 
     public static void PrintRectangle(int width, int height)
     {
-
+      for (int i = 0; i <= height; i++)
+      {
+        for (int x = 0; x <= width; x++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");  
+      }
     }
 
     public static void PrintTriangle(int size)
     {
-
+      for (int i = 1; i <= size; i++)
+      {
+        for (int x = 1; x <= i; x++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");  
+      }
     }
   }
 }
