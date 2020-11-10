4e56de5be121133cdbc0cd5607f41d5fe875401f
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-10 12:20:52 +0200

    10/11/2020

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -6,12 +6,25 @@ namespace exercise_53
   {
     public static void Main(String[] args)
     {
+      Console.WriteLine("What do you want to divide?");
+      int num = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("What do you want to divide it with?");
+      int den = Convert.ToInt32(Console.ReadLine());
+
       // Call your method here:
-
-
+      Division(num, den);
+      double div = Convert.ToDouble(Division);
+      Console.PrintLine("The result is " + div);
     }
 
     // Write your method here:
+    public static void Division(int num, int den)
+    {
+      int num = 1;
+      int den = 2;
+      int sum = num / den;
 
+      double div = Convert.ToDouble(sum);
+    }
   }
 }
