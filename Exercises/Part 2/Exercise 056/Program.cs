e212fb8e861f2c0746f02ec1425619afbb87e421
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-10 15:14:07 +0200

    10/11/2020

--- Exercises/Part 2/Exercise 056/Program.cs
+++ Exercises/Part 2/Exercise 056/Program.cs
@@ -6,10 +6,13 @@ namespace exercise_56
   {
     public static void Main(String[] args)
     {
-
+      Word();
     }
 
     // Write your method here:
-
+    public static string Word()
+    {
+      Console.WriteLine("Genier er som tordenvejr: de går mod vinden, forfærder menneskene og renser luften")
+    }
   }
 }
