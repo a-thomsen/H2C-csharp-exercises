743df5f1aa99c99295bb326fd1e6167b871f7c82
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-10 14:54:47 +0200

    10/11/2020

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -6,11 +6,26 @@ namespace exercise_54
   {
     public static void Main(String[] args)
     {
+      Console.WriteLine("Where do you want to start from?");
+      int start = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Where do you want to end up?");
+      int end = Convert.ToInt32(Console.ReadLine());
+
       // Call your method here:
+      DivisibleByThreeInRange(start, end);
 
     }
 
     // Write your method here:
-
+    public static void DivisibleByThreeInRange(int start, int end)
+    {
+      for (int i = start; i <= end; i++)
+      {
+        if ((i%3) == 0)
+        {
+          Console.WriteLine(i);
+        }
+      }
+    }
   }
 }
