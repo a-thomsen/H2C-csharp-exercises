f1ca8ccdf8ab161b210c9572b8af54f5faed4d80
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-18 16:13:50 +0200

    18/11/2020

--- Exercises/Part 3/Exercise 090/Program.cs
+++ Exercises/Part 3/Exercise 090/Program.cs
@@ -7,7 +7,34 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
+      // Ask user to enter name & age
 
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] data = input.Split(",");
+      }
+
+      // Compare ages to print the age of the oldest 
+
+      int oldest = Convert.ToInt32(data[1]);
+
+      for (int i = 0; i < data.Count; i++)
+      {
+        int age =  data[i];
+        if (oldest < age)
+        {
+          oldest = age;
+        }
+      }
+
+      // If empty line, break + print age of the oldest
+
+      Console.WriteLine("The age of the oldest is " + oldest);
 
     }
   }
