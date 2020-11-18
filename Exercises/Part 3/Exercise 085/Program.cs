0a5c9431cca055e97c68b80dbfde0ff1cac2ce91
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-18 12:46:05 +0200

    18/11/2020

--- Exercises/Part 3/Exercise 085/Program.cs
+++ Exercises/Part 3/Exercise 085/Program.cs
@@ -7,8 +7,27 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      string name = "alex emma";
+      string password = "sunshine haskell";
+      string[] pieces = text.Split(" ");
 
+      Console.WriteLine("Enter username");
+      string user = Console.ReadLine();
+      Console.WriteLine("Enter password");
+      string code = Console.ReadLine();
 
+      if (name[0] == user && password[0] == code)
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else if (name[1] == user && password[1] == code)
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else
+      {
+        Console.WriteLine("Incorrect username or password!");
+      }
     }
   }
 }
