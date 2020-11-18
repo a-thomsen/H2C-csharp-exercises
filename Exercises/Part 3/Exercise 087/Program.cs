c861fd7158a176f6738af741e54e9e67c3fdea06
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-18 13:12:46 +0200

    18/11/2020

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -7,10 +7,24 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+      while (true)
+      {
+        Console.WriteLine("Give a string");
+        string input = Console.ReadLine();
 
+        if(input != "")
+        {
+          string[] pieces = input.Split(" "); 
+
+          for(int i = 0; i < input.Length; i++)
+          {
+            if(input[i].Contains("av"))
+            {
+              Console.WriteLine(input[i]);
+            } 
+          }        
+        }
+      }
     }
-
   }
-
 }
-
