9a793d9b4a312e11d86ed5609b6b6f7a96e33824
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-18 12:54:50 +0200

    18/11/2020

--- Exercises/Part 3/Exercise 086/Program.cs
+++ Exercises/Part 3/Exercise 086/Program.cs
@@ -7,8 +7,22 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
+      while (true)
+      {
+        Console.WriteLine("Give a string");
+        string text = Console.ReadLine();
+        string[] pieces = text.Split(" ");
 
+        for (int i = 0; i < pieces.Length; i++)
+        {
+          Console.WriteLine(pieces[i]);
+        }
 
+        if (text == (""))
+        {
+          Console.WriteLine("Halted");
+        }
+      }
     }
   }
 }
