b73dc3f3a770181699dee9c713eb930ce230c5c0
Anna Thomsen <anna.thomsen@centria.fi>
2020-11-18 12:34:40 +0200

    18/11/2020

--- Exercises/Part 3/Exercise 084/Program.cs
+++ Exercises/Part 3/Exercise 084/Program.cs
@@ -7,8 +7,18 @@ namespace exercise_84
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give a string");
+      string input = Console.ReadLine();
 
-
+      if (input == "true")
+      {
+        Console.WriteLine("You got it right!");
+        break;
+      }
+      else
+      {
+        Console.WriteLine("Try again!");
+      }
     }
   }
 }
