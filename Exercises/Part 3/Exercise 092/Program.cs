7c092dbcf8091b233a93e434a98546515c72a73a
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-03 22:33:54 +0200

    03/12/2020

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,10 +7,45 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      int age = 0;
+      int birthYear = 0;
+      int oldest = 0;
 
-    }
+      string name = "";
+      string longestName = "";
+      
+      while (true)
+      {    
+        string input = Console.ReadLine();
+        
+        if (input != "")
+        {
+          string[] nameYOB = input.Split(',');
+
+          name = nameYOB[0];
+          birthYear = Convert.ToInt32(nameYOB[1]);
+          age = 2020 - birthYear;
+
+          if (age > oldest)
+          {
+              age = oldest;
+          }
+
+          if (name.Length > longestName.Length)
+          {
+            name = longestName;
+          }
+          else 
+          {
+            break;
+          }
+        }
+
+      Console.WriteLine("The longest name is " + longestName);
+      Console.WriteLine("The highest age is " + oldest);
+      }
+    }  
   }
 }
 
-
-
+// Hoooooly brain mayhem. Continue working on this baby girl!
