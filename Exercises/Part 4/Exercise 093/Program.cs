efc8d199cb2557560f9b9759c96032f725330fa1
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-14 22:46:58 +0200

    14/12/2020

--- Exercises/Part 4/Exercise 093/Program.cs
+++ Exercises/Part 4/Exercise 093/Program.cs
@@ -8,7 +8,10 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account annasAccount = new Account("Anna's account", 100.00);
 
+      annasAccount.Deposit(20);
+      Console.WriteLine("The balance of Anna's account is now: " + annasAccount.balance);
     }
   }
 }
