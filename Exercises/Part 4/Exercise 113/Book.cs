fbd8ebd7b84df6585fe78f3ba684983cb7129412
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 01:07:12 +0200

    15/11/2020

--- Exercises/Part 4/Exercise 113/Book.cs
+++ Exercises/Part 4/Exercise 113/Book.cs
@@ -3,19 +3,19 @@ using System;
 namespace exercise_113
 {
   public class Book {
-      public string name { get; set; }
+      public string title { get; set; }
       public int pages { get; set; } 
       public int year { get; set; }
 
-  public Book (string name){
+  public Book (string title){
       this.pages = pages;
       this.year = year; 
-      this.name = name;
+      this.title = title;
     }
 
   public override string ToString()
     {
-        return this.name + ", " + this.pages + " pages, " + this.year;
+        return this.title + ", " + this.pages + " pages, " + this.year;
     }  
   }
 }
