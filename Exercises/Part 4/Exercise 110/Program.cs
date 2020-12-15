5c3a964060b9e0bf2f188bbceef6b040753b9242
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 00:14:28 +0200

    15/11/2020

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -8,6 +8,23 @@ namespace exercise_110
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+      List<Item> items = new List<Item>();
+
+      while (true)
+      {
+        Console.Write("Enter a name, empty will stop: ");
+        String name = Console.ReadLine();
+          if (name == ""){
+            break;
+          } 
+          else {
+            items.Add(new Item(name));
+          }
+      }  
+        
+      for(int i < 0; i < items.Count; i++){
+        Console.WriteLine(items[i].ToString());
+      }
     }
   }
 }
