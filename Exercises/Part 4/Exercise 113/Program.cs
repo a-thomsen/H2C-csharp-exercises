f2d7fb8728037c8df30f4d438d90a9bcd5d3b6b8
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 01:22:11 +0200

    15/12/2020

--- Exercises/Part 4/Exercise 113/Program.cs
+++ Exercises/Part 4/Exercise 113/Program.cs
@@ -10,9 +10,48 @@ namespace exercise_113
       // IMPLEMENT YOUR CODE IN HERE!
       // Remember also to create the file Book.cs and class in it!
 
+    List<Book> books = new List<Book>();
+
+    while(true){
+
+        Console.Write("Enter the title of the book, number of pages and the year it was published separated with a comma");
+        string details = Console.ReadLine(); 
+
+          if (details == ""){
+            break; 
+          }
+
+        string[]parts = details.Split(",");
+        string title = parts[0];
+        int pages  = Convert.ToInt32(parts[1]);
+        int year = Convert.ToInt32(parts[2]);
+
+        books.Add(new Book(title, pages, year)); 
+      }
+
+    while(true){  
+
+        Console.WriteLine("What do you want to print?");
+        string print = Console.ReadLine(); 
+
+          if (print == "everything"){
+            for (int i = 0; i < books.Count; i++){
+              Console.WriteLine(books[i].ToString());
+            }
+          }  
+          else if (print == "title"){
+            for (int i = 0; i < books.Count; i++){
+              Console.WriteLine(books[i].title); 
+            }
+          }  
+          else {
+            break; 
+          }
+      }
     }
   }
 }
+
 
 
 
