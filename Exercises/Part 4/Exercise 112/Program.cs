608a554030f3bfd591f519038266ff8f18119dc3
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 00:51:15 +0200

    15/11/2020

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -9,11 +9,33 @@ namespace exercise_112
     {
       // IMPLEMENT YOUR CODE IN HERE!
 
+      List<TelevisionProgram> programs = new List<TelevisionProgram>();
+      
+      while (true)
+      {
+        Console.WriteLine("Enter a TV program");
+        string name = Console.ReadLine();
+          if (name == "")
+          {
+            break;
+          }
 
+        Console.Write("Enter the duration of " + name);
+        int duration = Convert.ToInt32(Console.ReadLine());
+
+        programs.Add(new TelevisionProgram(name, duration)); 
+      }
+
+      Console.WriteLine(); 
+      Console.WriteLine("Enter the maximum duration of the program");
+      int maxLimit = Convert.ToInt32(Console.ReadLine()); 
+
+      for (int i = 0; i < programs.Count; i++) {
+        
+        if (programs[i].duration <= maxLimit) {
+          Console.WriteLine(program[i].ToString()); 
+        }
+      }
     }
-  }
-}
-
-
-
-
+  }  
+}  
