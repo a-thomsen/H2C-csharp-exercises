6e731fef462a9fdc79286d7cedbaec3592fb65f1
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 00:28:53 +0200

    15/12/2020

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -10,6 +10,28 @@ namespace exercise_111
       // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
 
+      while(true){
+
+        Console.Write("Enter your first name, last name and identification number separated with a comma");
+        string details = Console.ReadLine(); 
+
+        if (details == ""){
+          break; 
+        }
+
+        string[]parts = details.Split(",");
+        string firstName = parts[0];
+        string lastName = parts[1];
+        int idNumber = Convert.ToInt32(parts[2]);
+
+        list.Add(new PersonalInformation(firstName, lastName, idNumber)); 
+      }
+
+      Console.WriteLine();
+
+      foreach (PersonalInformation personalInformation in list) {
+      Console.WriteLine(personalInformation.firstName + " " + personalInformation.lastName);
+      }
     }
   }
 }
