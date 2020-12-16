fa75c15e10fc8cae16b5fa838359cbe2aeac089f
a-thomsen <73801786+a-thomsen@users.noreply.github.com>
2020-12-17 01:22:10 +0200

    16/12/2020

--- Exercises/Part 5/Exercise 121/Fitbyte.cs
+++ Exercises/Part 5/Exercise 121/Fitbyte.cs
@@ -2,23 +2,20 @@ using System;
 
 namespace exercise_121
 {
-    public class Fitbyte
-    {
+    public class Fitbyte {
         public int age { get; set; }
         public int restingHeartRate { get; set; }
         public double maxHeartRate { get; set; }
 
-        public Fitbyte(int age, int restingHeartRate)
-        {
-            this.age = age;
-            this.restingHeartRate = restingHeartRate;
-            this.maxHeartRate = 206.3 - (0.711 * this.age);
+        public Fitbyte(int age, int restingHeartRate) {
+          this.age = age;
+          this.restingHeartRate = restingHeartRate;
+          this.maxHeartRate = 206.3 - (0.711 * this.age);
         }
 
-        public double TargetHeartRate(double percentageOfMaximum)
-        {
-            double targetRate = (this.maxHeartRate - this.restingHeartRate) * percentageOfMaximum + this.restingHeartRate;
-            return targetRate;
+        public double TargetHeartRate(double percentageOfMaximum) {
+          double targetRate = (this.maxHeartRate - this.restingHeartRate) * percentageOfMaximum + this.restingHeartRate;
+          return targetRate;
         }
     }
 }
