0ac1d9cd87927d40a1b10dc0613ba20dcf49be94
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 23:03:42 +0200

    16/11/2020

--- Exercises/Part 5/Exercise 123/Counter.cs
+++ Exercises/Part 5/Exercise 123/Counter.cs
@@ -2,11 +2,34 @@ using System;
 
 namespace exercise_123
 {
-  public class Product
+  public class Counter
   {
     // DO NOT TOUCH THESE VARIABLES
-    private string name;
-    private string location;
-    private int weight;
+    public int value {get; set;}
 
     // CREATE YOUR CONSTRUCTORS HERE
+    public Counter (int startValue){
+        this.value = startValue;
+    }
+
+    public Counter (){
+        this.value = 0; 
+    }
+
+    public void Increase(){
+        this.value++;
+    }
+
+    public void Increase(int increaseBy){
+        this.value += increaseBy;
+    }
+
+    public void Decrease(){
+        this.value--;
+    }
+
+    public void Decrease(int decreaseBy){
+        this.value -= decreaseBy;
+    }
+  }
+}    
