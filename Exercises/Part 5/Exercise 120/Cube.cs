7da9ad3c8a790ad2e282d9ae50235d434f7841a6
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 21:56:39 +0200

    16/11/2020

---
+++ Exercises/Part 5/Exercise 120/Cube.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Cube
+{
+	public int edge { get; set; }
+
+	public Cube(int length)
+	{
+		this.edge = length;
+	}
+
+	public int Volume()
+    {
+		int volume = this.edge * this.edge * this.edge;
+		return volume;
+    }
+
+    public override string ToString()
+    {
+		return "The length of the edge is " + this.edge + " and the volume is " + this.Volume();
+    }
+}
