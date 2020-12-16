eb320948c488b25ffebcf6e500480cb79381b05e
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 17:41:56 +0200

    16/12/2020

--- Exercises/Part 4/Exercise 114/Program.cs
+++ Exercises/Part 4/Exercise 114/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
+          int count = 0; 
 
+          while (true) {
+            string line = Console.ReadLine();
+
+                if (line == "end") {
+                break;
+                }
+            count++; 
+          }
+        Console.WriteLine("Strings read: " + count); 
         }
     }
 }
