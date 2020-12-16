656b61c84da981e6fbedc92134834b47e57ec8d9
a-thomsen <73801786+a-thomsen@users.noreply.github.com>
2020-12-17 01:21:16 +0200

    16/12/2020

--- Exercises/Part 5/Exercise 120/Cube.cs
+++ Exercises/Part 5/Exercise 120/Cube.cs
@@ -4,22 +4,19 @@ namespace exercise_120
 {
 	public class Cube
 	{
-		public int edge { get; set; }
+	  public int edge { get; set; }
 
-		public Cube(int length)
-		{
-			this.edge = length;
-		}
+	  public Cube(int length){
+	    this.edge = length;
+	  }
 
-		public int Volume()
-		{
-			int volume = this.edge * this.edge * this.edge;
-			return volume;
-		}
-
-		public override string ToString()
-		{
-			return "The length of the edge is " + this.edge + " and the volume is " + this.Volume();
-		}
+	public int Volume(){
+	    int volume = this.edge * this.edge * this.edge;
+	    return volume;
 	}
+
+	public override string ToString(){
+	    return "The length of the edge is " + this.edge + " and the volume is " + this.Volume();
+	}
+    }
 }
