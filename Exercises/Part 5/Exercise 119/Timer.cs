2c9a818c6c5df242f046686baf81922fa1743c22
Anna Thomsen <anna.thomsen@centria.fi>
2020-12-16 21:47:17 +0200

    16/11/2020

---
+++ Exercises/Part 5/Exercise 119/Timer.cs
@@ -0,0 +1,29 @@
+public class Timer
+{
+  public int hundredths {get; set;}
+  public int seconds {get; set;}
+
+  public Timer()
+  {
+    this.hundredths = 0;
+    this.seconds = 0;
+  }
+
+  public void Advance()
+  {
+    this.hundredths = this.hundredths + 1; 
+    
+    if (this.hundredths >= 100){
+        this.hundredths = 0;
+        this.seconds = this.seconds + 1; 
+    }
+    if (this.seconds >= 60){
+        this.seconds = 0; 
+    }
+  }
+
+  public override string ToString()
+  {
+    return this.seconds + ":" + this.hundredths; 
+  }
+}
